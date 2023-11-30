44e72be1cf3f60704837d5dd3a2fd80dfe00d8ca
Kian Shirvan <spoono17@gmail.com>
2023-11-29 23:52:12 -0700

    Added "luxury train" option, added debug mode, added custom stops area to config and logic for custom stops

--- client/client.lua
+++ client/client.lua
@@ -6,16 +6,15 @@ local westBlipRendered = false
 local eastBlipRendered = false
 local eastTrainDriver = nil
 local westTrainDriver = nil
+local tramDriver = nil
 local Trains = {}
 local TrainModels = {
     'northsteamer01x'
 }
 local christmasTrainHash = 0x124A1F89
-
--- Starting locations
-local loc = vector3(2590.34, -1477.24, 45.86)
-local loc2 = vector3(2608.38, -1203.12, 53.16)
-local loc3 = vector3(-3766.98, -2787.98, -14.44)
+local barPropsHash = -1747631964
+local sleeperPropsHash = -317994478
+local object
 
 -- Check for server stored train variables
 RegisterNetEvent("BGS_Trains:GetServerTrains")
@@ -154,6 +153,8 @@ local function TramCreateVehicle(trainModel, loc)
 		Citizen.Wait(1)
 	end
 
+	tramDriver = trainDriverHandle
+
 	-- Make driver invincible
 	SetEntityAsMissionEntity(trainDriverHandle, true, true)
 	SetEntityCanBeDamaged(trainDriverHandle, false)
@@ -217,42 +218,49 @@ local function IsTrain(vehicle)
 end
 
 local function DecorateTrain(vehicle)
-    local object = CreateObjectNoOffset(GetHashKey('mp006_p_veh_xmasnsteamer01x'), 0, 0, 0, false, false, false, false)
+    object = CreateObjectNoOffset(GetHashKey('mp006_p_veh_xmasnsteamer01x'), 0, 0, 0, false, false, false, false)
     AttachEntityToEntity(object, vehicle, 0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, false, false, false, false, 0, true, false, false)
     return object
 end
 
 -- Spawn and store trains server side
-RegisterNetEvent("vorp:SelectedCharacter", function()
-	TriggerServerEvent("BGS_Trains:ReturnServerTrains")
-	Wait(1000)
-	while eastTrain == nil and tram == nil and westTrain == nil do
+if not Config.Debug then
+	RegisterNetEvent("vorp:SelectedCharacter", function()
+		TriggerServerEvent("BGS_Trains:ReturnServerTrains")
 		Wait(1000)
-		if Config.UseTrams then
-			TramCreateVehicle(Config.Trolley, loc2)
-			TriggerServerEvent("BGS_Trains:StoreServerTram", tram)
-		end
-		if Config.UseEastTrain then
-			if Config.UseChristmasTrainEast then
-				EastTrainCreateVehicle(christmasTrainHash, loc, Config.EastTrainMaxSpeed)
-			else
-				EastTrainCreateVehicle(Config.EastTrain, loc, Config.EastTrainMaxSpeed)
+		while eastTrain == nil and tram == nil and westTrain == nil and not Config.Debug do
+			Wait(1000)
+			if Config.UseTrams then
+				TramCreateVehicle(Config.Trolley, Config.tramLoc)
+				TriggerServerEvent("BGS_Trains:StoreServerTram", tram)
 			end
-			TriggerServerEvent("BGS_Trains:StoreServerTrainEast", eastTrain)
-		end
-		if Config.UseWestTrain then
-			if Config.UseChristmasTrainWest then
-				WestTrainCreateVehicle(christmasTrainHash, loc3, Config.WestTrainMaxSpeed)
-			else
-				WestTrainCreateVehicle(Config.WestTrain, loc3, Config.WestTrainMaxSpeed)
+			if Config.UseEastTrain then
+				if Config.UseChristmasTrainEast then
+					EastTrainCreateVehicle(christmasTrainHash, Config.eastLoc, Config.EastTrainMaxSpeed)
+				elseif Config.UseFancyTrainEast then
+					EastTrainCreateVehicle(0xCD2C7CA1, Config.eastLoc, Config.EastTrainMaxSpeed)
+				else
+					EastTrainCreateVehicle(Config.EastTrain, Config.eastLoc, Config.EastTrainMaxSpeed)
+				end
+				TriggerServerEvent("BGS_Trains:StoreServerTrainEast", eastTrain)
 			end
-			TriggerServerEvent("BGS_Trains:StoreServerTrainWest", westTrain)
+			if Config.UseWestTrain then
+				if Config.UseChristmasTrainWest then
+					WestTrainCreateVehicle(christmasTrainHash, Config.westLoc, Config.WestTrainMaxSpeed)
+				elseif Config.UseFancyTrainWest then
+					WestTrainCreateVehicle(0xCD2C7CA1, Config.westLoc, Config.WestTrainMaxSpeed)
+				else
+					WestTrainCreateVehicle(Config.WestTrain, Config.westLoc, Config.WestTrainMaxSpeed)
+				end
+				TriggerServerEvent("BGS_Trains:StoreServerTrainWest", westTrain)
+			end
 		end
-	end
-end)
+	end)
+end
 
 -- Handle west train shit
 CreateThread(function()
+	local stopped = false
 	while true do
 		Wait(500)
 		if westTrain then
@@ -270,11 +278,24 @@ CreateThread(function()
 				Wait(Config.WestTrainStationWait*1000)
 				Citizen.InvokeNative(0x787E43477746876F, westTrain)
 			end
+			for index, stop in ipairs(Config.CustomStops) do
+				if GetDistanceBetweenCoords(stop.coords, GetEntityCoords(westTrain)) < 15 and not stopped then
+					Citizen.InvokeNative(0x3660BCAB3A6BB734, westTrain)
+					Wait(Config.WestTrainStationWait*1000)
+					Citizen.InvokeNative(0x787E43477746876F, westTrain)
+					stopped = true
+				end
+				if GetDistanceBetweenCoords(stop.coords, GetEntityCoords(westTrain)) > 15 then
+					stopped = false
+				end
+			end
 		end
 	end
 end)
 
+-- Handle east train shit
 CreateThread(function()
+	local stopped = false
 	while true do
 		Wait(500)
 		if eastTrain then
@@ -298,6 +319,17 @@ CreateThread(function()
 				Citizen.InvokeNative(0x3660BCAB3A6BB734, eastTrain)
 				Wait(Config.EastTrainStationWait*1000)
 				Citizen.InvokeNative(0x787E43477746876F, eastTrain)
+			end
+			for index, stop in ipairs(Config.CustomStops) do
+				if GetDistanceBetweenCoords(stop.coords, GetEntityCoords(eastTrain)) < 15 and not stopped then
+					Citizen.InvokeNative(0x3660BCAB3A6BB734, eastTrain)
+					Wait(Config.EastTrainStationWait*1000)
+					Citizen.InvokeNative(0x787E43477746876F, eastTrain)
+					stopped = true
+				end
+				if GetDistanceBetweenCoords(stop.coords, GetEntityCoords(eastTrain)) > 15 then
+					stopped = false
+				end
 			end
 		end
 	end
@@ -345,9 +377,10 @@ CreateThread(function()
 	end
 end)
 
+-- Handle Christmas train shit
 CreateThread(function()
     while true do
-        Wait(100)
+        Wait(1000)
 
 		if Config.UseChristmasTrainEast or Config.UseChristmasTrainWest then
 			for vehicle in EnumerateVehicles() do
@@ -362,7 +395,7 @@ CreateThread(function()
 					end
 				end
 			end
-	
+
 			for train, object in pairs(Trains) do
 				if not DoesEntityExist(train) then
 					DeleteEntity(object)
@@ -372,3 +405,210 @@ CreateThread(function()
 		end
     end
 end)
+
+-- Handle "luxury train" shit
+if Config.UseFancyTrainEast then
+	CreateThread(function()
+		local deleted = false
+		local attached = false
+		local propset
+		local propset2
+		local propsetHash
+		local propsetHash2
+		local barPropsetOnTrain
+		local sleeperPropsetOnTrain
+
+		while true do
+			Wait(1000)
+			if eastTrain then
+
+				local carriage = Citizen.InvokeNative(0xD0FB093A4CDB932C, eastTrain, 3)
+				local carriage2 = Citizen.InvokeNative(0xD0FB093A4CDB932C, eastTrain, 4)
+				local carriage3 = Citizen.InvokeNative(0xD0FB093A4CDB932C, eastTrain, 6)
+				local carriage4 = Citizen.InvokeNative(0xD0FB093A4CDB932C, eastTrain, 5)
+
+				if GetDistanceBetweenCoords(GetEntityCoords(PlayerPedId()), GetEntityCoords(carriage4)) < 150 then
+					propset = Citizen.InvokeNative(0xCFC0BD09BB1B73FF, carriage)
+					propsetHash = Citizen.InvokeNative(0xA6A9712955F53D9C, propset)
+
+					propset2 = Citizen.InvokeNative(0xCFC0BD09BB1B73FF, carriage2)
+					propsetHash2 = Citizen.InvokeNative(0xA6A9712955F53D9C, propset2)
+
+					if Citizen.InvokeNative(0xF42DB680A8B2A4D9, propset) and (Citizen.InvokeNative(0xF42DB680A8B2A4D9, propset2)) and not deleted then
+						while not (Citizen.InvokeNative(0x48A88FC684C55FDC, propsetHash) and Citizen.InvokeNative(0x48A88FC684C55FDC, propsetHash2)) do
+							Wait(1)
+						end
+						Citizen.InvokeNative(0x58AC173A55D9D7B4, propset, true, true)
+						Citizen.InvokeNative(0x58AC173A55D9D7B4, propset2, true, true)
+						deleted = true
+					end
+
+					if not attached then
+
+						local request = Citizen.InvokeNative(0xF3DE57A46D5585E9, barPropsHash)
+						local request2 = Citizen.InvokeNative(0xF3DE57A46D5585E9, sleeperPropsHash)
+
+						while not Citizen.InvokeNative(0x48A88FC684C55FDC, barPropsHash) do
+							Wait(1)
+						end
+
+						while not Citizen.InvokeNative(0x48A88FC684C55FDC, sleeperPropsHash) do
+							Wait(1)
+						end
+
+						barPropsetOnTrain = Citizen.InvokeNative(0x9609DBDDE18FAD8C, barPropsHash, 0, 0, 0, carriage, 0, true, 0, true)
+						sleeperPropsetOnTrain = Citizen.InvokeNative(0x9609DBDDE18FAD8C, sleeperPropsHash, 0, 0, 0, carriage2, 0, true, 0, true)
+						attached = true
+					end
+
+					Citizen.InvokeNative(0xB1964A83B345B4AB, barPropsHash)
+					Citizen.InvokeNative(0xB1964A83B345B4AB, sleeperPropsHash)
+
+				else
+					attached = false
+				end
+
+				Citizen.InvokeNative(0x550CE392A4672412, carriage3, 9, true, true)			-- Open fancy cabin doors
+				Citizen.InvokeNative(0x550CE392A4672412, carriage3, 10, true, true)			-- Open fancy cabin doors
+				Citizen.InvokeNative(0x550CE392A4672412, carriage3, 11, true, true)			-- Open fancy cabin doors
+
+				Citizen.InvokeNative(0x550CE392A4672412, carriage4, 9, true, true)			-- Open fancy cabin doors
+				Citizen.InvokeNative(0x550CE392A4672412, carriage4, 10, true, true)			-- Open fancy cabin doors
+				Citizen.InvokeNative(0x550CE392A4672412, carriage4, 11, true, true)			-- Open fancy cabin doors
+				
+			end
+		end
+	end)
+end
+
+if Config.UseFancyTrainWest then
+	CreateThread(function()
+		local deleted = false
+		local attached = false
+		local propset
+		local propset2
+		local propsetHash
+		local propsetHash2
+		local barPropsetOnTrain
+		local sleeperPropsetOnTrain
+
+		while true do
+			Wait(1000)
+			if eastTrain then
+
+				local carriage = Citizen.InvokeNative(0xD0FB093A4CDB932C, westTrain, 3)
+				local carriage2 = Citizen.InvokeNative(0xD0FB093A4CDB932C, westTrain, 4)
+				local carriage3 = Citizen.InvokeNative(0xD0FB093A4CDB932C, westTrain, 6)
+				local carriage4 = Citizen.InvokeNative(0xD0FB093A4CDB932C, westTrain, 5)
+
+				if GetDistanceBetweenCoords(GetEntityCoords(PlayerPedId()), GetEntityCoords(carriage4)) < 150 then
+					propset = Citizen.InvokeNative(0xCFC0BD09BB1B73FF, carriage)
+					propsetHash = Citizen.InvokeNative(0xA6A9712955F53D9C, propset)
+
+					propset2 = Citizen.InvokeNative(0xCFC0BD09BB1B73FF, carriage2)
+					propsetHash2 = Citizen.InvokeNative(0xA6A9712955F53D9C, propset2)
+
+					if Citizen.InvokeNative(0xF42DB680A8B2A4D9, propset) and (Citizen.InvokeNative(0xF42DB680A8B2A4D9, propset2)) and not deleted then
+						while not (Citizen.InvokeNative(0x48A88FC684C55FDC, propsetHash) and Citizen.InvokeNative(0x48A88FC684C55FDC, propsetHash2)) do
+							Wait(1)
+						end
+						deleted = true
+						Citizen.InvokeNative(0x58AC173A55D9D7B4, propset, true, true)
+						Citizen.InvokeNative(0x58AC173A55D9D7B4, propset2, true, true)
+					end
+					local request = Citizen.InvokeNative(0xF3DE57A46D5585E9, barPropsHash)
+					local request2 = Citizen.InvokeNative(0xF3DE57A46D5585E9, sleeperPropsHash)
+
+					while not Citizen.InvokeNative(0x48A88FC684C55FDC, barPropsHash) do
+						Wait(1)
+					end
+
+					while not Citizen.InvokeNative(0x48A88FC684C55FDC, sleeperPropsHash) do
+						Wait(1)
+					end
+
+					if not attached then
+						barPropsetOnTrain = Citizen.InvokeNative(0x9609DBDDE18FAD8C, barPropsHash, 0, 0, 0, carriage, 0, true, 0, true)
+						sleeperPropsetOnTrain = Citizen.InvokeNative(0x9609DBDDE18FAD8C, sleeperPropsHash, 0, 0, 0, carriage2, 0, true, 0, true)
+						attached = true
+					end
+
+					Citizen.InvokeNative(0xB1964A83B345B4AB, barPropsHash)
+					Citizen.InvokeNative(0xB1964A83B345B4AB, sleeperPropsHash)
+
+				else
+					attached = false
+				end
+
+				Citizen.InvokeNative(0x550CE392A4672412, carriage3, 9, true, true)			-- Open fancy cabin doors
+				Citizen.InvokeNative(0x550CE392A4672412, carriage3, 10, true, true)			-- Open fancy cabin doors
+				Citizen.InvokeNative(0x550CE392A4672412, carriage3, 11, true, true)			-- Open fancy cabin doors
+
+				Citizen.InvokeNative(0x550CE392A4672412, carriage4, 9, true, true)			-- Open fancy cabin doors
+				Citizen.InvokeNative(0x550CE392A4672412, carriage4, 10, true, true)			-- Open fancy cabin doors
+				Citizen.InvokeNative(0x550CE392A4672412, carriage4, 11, true, true)			-- Open fancy cabin doors
+				
+			end
+		end
+	end)
+end
+
+if Config.Debug then
+
+	if Config.UseTrams then
+		TramCreateVehicle(Config.Trolley, Config.tramLoc)
+		TriggerServerEvent("BGS_Trains:StoreServerTram", tram)
+	end
+
+	if Config.UseEastTrain then
+		if Config.UseChristmasTrainEast then
+			EastTrainCreateVehicle(christmasTrainHash, Config.eastLoc, Config.EastTrainMaxSpeed)
+		elseif Config.UseFancyTrainEast then
+			EastTrainCreateVehicle(0xCD2C7CA1, Config.eastLoc, Config.EastTrainMaxSpeed)
+		else
+			EastTrainCreateVehicle(Config.EastTrain, Config.eastLoc, Config.EastTrainMaxSpeed)
+		end
+		TriggerServerEvent("BGS_Trains:StoreServerTrainEast", eastTrain)
+	end
+
+	AddEventHandler('onResourceStop', function(resourceName)
+		if GetCurrentResourceName() == resourceName then
+			if Config.UseChristmasTrainEast or Config.UseChristmasTrainWest then
+				DeleteEntity(object)
+			end
+			NetworkRequestControlOfEntity(westTrain)
+			NetworkRequestControlOfEntity(eastTrain)
+			NetworkRequestControlOfEntity(tram)
+			NetworkRequestControlOfEntity(westTrainDriver)
+			NetworkRequestControlOfEntity(eastTrainDriver)
+			NetworkRequestControlOfEntity(tramDriver)
+			DeleteEntity(westTrain)
+			DeleteEntity(eastTrain)
+			DeleteEntity(tram)
+			DeleteEntity(westTrainDriver)
+			DeleteEntity(eastTrainDriver)
+			DeleteEntity(tramDriver)
+			eastTrain = nil
+			westTrain = nil
+			tram = nil
+			eastTrainDriver = nil
+			westTrainDriver = nil
+			tramDriver = nil
+		end
+	end)
+
+	CreateThread(function()
+		while true do
+			Wait(10)
+			if tram then
+				Citizen.InvokeNative(0x3660BCAB3A6BB734, tram)
+			end
+			if eastTrain then
+				Citizen.InvokeNative(0x3660BCAB3A6BB734, eastTrain)
+			end
+			if westTrain then
+				Citizen.InvokeNative(0x3660BCAB3A6BB734, westTrain)
+			end
+		end
+	end)
+end
