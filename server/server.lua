fb1bd7170c46cf337b0e365030290a6076b0f5b8
RexshackGaming <rexshackuk@gmail.com>
2024-01-30 19:08:06 +0000

    clean-up

--- server/server.lua
+++ server/server.lua
@@ -10,68 +10,68 @@ local tram
 local tramConductor
 
 RegisterServerEvent("BGS_Trains:server:CanSpawnTrain", function ()
-	local src = source
-	local canSpawn = false
+    local src = source
+    local canSpawn = false
 
-	table.insert(players, src)
+    table.insert(players, src)
 
-	if not spawned then
-		spawned = true
-		canSpawn = true
-	end
+    if not spawned then
+        spawned = true
+        canSpawn = true
+    end
 
-	Wait(1000)
+    Wait(1000)
 
-	TriggerClientEvent("BGS_Trains:client:CanSpawnTrain", src, canSpawn)
+    TriggerClientEvent("BGS_Trains:client:CanSpawnTrain", src, canSpawn)
 end)
 
 RegisterServerEvent("BGS_Trains:server:StoreNetIndex", function (trainNetIndex, driverNetIndex, trainArea)
-	if trainArea == "east" then
-		eastTrain = trainNetIndex
-		eastConductor = driverNetIndex
-	elseif trainArea == "west" then
-		westTrain = trainNetIndex
-		westConductor = driverNetIndex
-	else
-		tram = trainNetIndex
-		tramConductor = driverNetIndex
-	end
+    if trainArea == "east" then
+        eastTrain = trainNetIndex
+        eastConductor = driverNetIndex
+    elseif trainArea == "west" then
+        westTrain = trainNetIndex
+        westConductor = driverNetIndex
+    else
+        tram = trainNetIndex
+        tramConductor = driverNetIndex
+    end
 end)
 
 RegisterServerEvent("BGS_Trains:server:GetTrainsFromServer", function ()
-	local src = source
-	TriggerClientEvent("BGS_Trains:client:GetTrainsFromServer", src, eastTrain, westTrain, tram, eastConductor, westConductor, tramConductor)
+    local src = source
+    TriggerClientEvent("BGS_Trains:client:GetTrainsFromServer", src, eastTrain, westTrain, tram, eastConductor, westConductor, tramConductor)
 end)
 
 AddEventHandler("playerDropped", function(reason)
-	local _source = source
-	for index, player in ipairs(players) do
-		if player == _source then
-			table.remove(players, index)
-			break
-		end
-	end
-	if #players < 1 then
-		spawned = false
-		DeleteEntity(NetworkGetEntityFromNetworkId(eastTrain))
-		DeleteEntity(NetworkGetEntityFromNetworkId(eastConductor))
-		DeleteEntity(NetworkGetEntityFromNetworkId(westTrain))
-		DeleteEntity(NetworkGetEntityFromNetworkId(westConductor))
-		DeleteEntity(NetworkGetEntityFromNetworkId(tram))
-		DeleteEntity(NetworkGetEntityFromNetworkId(tramConductor))
-		eastTrain, eastConductor, westTrain, westConductor, tram, tramConductor = nil, nil, nil, nil, nil, nil
-	end
+    local _source = source
+    for index, player in ipairs(players) do
+        if player == _source then
+            table.remove(players, index)
+            break
+        end
+    end
+    if #players < 1 then
+        spawned = false
+        DeleteEntity(NetworkGetEntityFromNetworkId(eastTrain))
+        DeleteEntity(NetworkGetEntityFromNetworkId(eastConductor))
+        DeleteEntity(NetworkGetEntityFromNetworkId(westTrain))
+        DeleteEntity(NetworkGetEntityFromNetworkId(westConductor))
+        DeleteEntity(NetworkGetEntityFromNetworkId(tram))
+        DeleteEntity(NetworkGetEntityFromNetworkId(tramConductor))
+        eastTrain, eastConductor, westTrain, westConductor, tram, tramConductor = nil, nil, nil, nil, nil, nil
+    end
 end)
 
 AddEventHandler('onResourceStop', function(resourceName)
-	if GetCurrentResourceName() == resourceName then
+    if GetCurrentResourceName() == resourceName then
         spawned = false
-		DeleteEntity(NetworkGetEntityFromNetworkId(eastTrain))
-		DeleteEntity(NetworkGetEntityFromNetworkId(eastConductor))
-		DeleteEntity(NetworkGetEntityFromNetworkId(westTrain))
-		DeleteEntity(NetworkGetEntityFromNetworkId(westConductor))
-		DeleteEntity(NetworkGetEntityFromNetworkId(tram))
-		DeleteEntity(NetworkGetEntityFromNetworkId(tramConductor))
-		players = {}
-	end
+        DeleteEntity(NetworkGetEntityFromNetworkId(eastTrain))
+        DeleteEntity(NetworkGetEntityFromNetworkId(eastConductor))
+        DeleteEntity(NetworkGetEntityFromNetworkId(westTrain))
+        DeleteEntity(NetworkGetEntityFromNetworkId(westConductor))
+        DeleteEntity(NetworkGetEntityFromNetworkId(tram))
+        DeleteEntity(NetworkGetEntityFromNetworkId(tramConductor))
+        players = {}
+    end
 end)
